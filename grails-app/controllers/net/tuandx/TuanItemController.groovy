c7a4a0112ed109326b4bc914f60634f866b10e25
ZhiminWu <babatu@gmail.com>
2010-07-01 20:13:01 +0800

    init

---
+++ grails-app/controllers/net/tuandx/TuanItemController.groovy
@@ -0,0 +1,15 @@
+/**
+ *Alipay.com Inc.
+ * Copyright (c) 2005-20010 All Rights Reserved.
+ */
+package net.tuandx
+
+/**
+ *	
+ * @author zuomou
+ *
+ * @version  TuanItemController.groovy v 0.1 2010-7-1 下午04:20:43  zuomou Exp 
+ */
+class TuanItemController {
+    def scaffold = TuanItem
+}
